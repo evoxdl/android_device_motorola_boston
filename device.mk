ed09d633c864eb89762e4925c74c46bbc9cf4715
Michael Bestas <mkbestas@lineageos.org>
2025-04-30 23:06:51 +0300

    boston: Build FM

--- device.mk
+++ device.mk
@@ -54,6 +54,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.moto_sm6450_fod
 
+# FM
+TARGET_HAS_FM := true
+
+PRODUCT_PACKAGES += \
+    FM2
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
