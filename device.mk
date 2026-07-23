84dc8809cd713bc07f63a696ffe6d8d773952fc6
Nick Reuter <nreuter85@gmail.com>
2026-07-22 23:01:46 -0500

    Revert "avatrn: Decommonize fstab"
    This reverts commit dec3bce8127c2c8b187d3d791f31080f7015d6a4.
    
    Change-Id: I8fddcd490c281fba5d490b577a9b258bea8c07df

--- device.mk
+++ device.mk
@@ -62,8 +62,6 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_PACKAGES += \
-    fstab.qcom \
-    fstab.qcom.vendor_ramdisk \
     init.mmi.overlay.rc \
     init.vendor.st21nfc.rc \
 	init.oem.fingerprint2.sh
